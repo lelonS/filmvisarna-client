2c75a2162925a68b04cef524ed8195cdea7ed751
Teddymannen <teddy_kross@hotmail.com>
2023-12-12 15:30:11 +0100

    Create CancelBooking.test.jsx

---
+++ src/components/userPage/CancelBooking.test.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import ConfirmBooking from './ConfirmBooking';
