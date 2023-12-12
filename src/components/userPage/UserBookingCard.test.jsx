ababed0b5ccf3187f389e74c8633256536942bb6
lelonS <35070147+lelonS@users.noreply.github.com>
2023-12-12 15:28:45 +0100

    Create UserBookingCard.test.jsx

---
+++ src/components/userPage/UserBookingCard.test.jsx
@@ -0,0 +1,19 @@
+import React, { useState as useStateMock } from 'react'
+import { render, screen, fireEvent } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import UserBookingCard from './UserBookingCard';
+
+
+// jest.mock('react', () => ({
+//   ...jest.requireActual('react'),
+//   useState: jest.fn(),
+// }))
+
+
+
+
+
+describe('UserBookingCard', () => {
+
+
+});
